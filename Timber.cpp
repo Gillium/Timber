efb4eef0f5b9737339a632a4c08648209f0c739e
Peter Piper <petermpiper@gmail.com>
2020-05-02 19:27:22 -0400

    included libraries and added code
    Included library for graphics and created a video mode object and a window for the game

--- Timber.cpp
+++ Timber.cpp
@@ -1,20 +1,16 @@
-// Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Include important libraries here
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+// Make code easier to type with 'using namespace"
+using namespace sf;
 
+// This is where our game starts from
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
