ae27c62026408f398ca90519c18a409304d77503
Peter Piper <petermpiper@gmail.com>
2020-05-03 17:56:29 -0400

    added tree, 3 clouds and a bee
    Added a tree, 3 clouds and a bee sprites. Loaded there graphics and set the positions of all the sprites. Set the speed of the clouds and bee initially to zero and set there moving activities to initially false. Drew all sprites to the scene

--- Timber.cpp
+++ Timber.cpp
@@ -11,6 +11,7 @@ int main()
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	
 	// Create a texture to hold a graphic on the GPU
 	Texture textureBackground;
 	// Load a graphic into the texture
@@ -21,6 +22,49 @@ int main()
 	spriteBackground.setTexture(textureBackground);
 	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(0, 0);
+	
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+	
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+	// Is the bee currently moving?
+	bool beeActive = false;
+	// How fast can the bee fly
+	float beeSpeed = 0.0f;
+
+	// Make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+	// Load 1 new texture
+	textureCloud.loadFromFile("graphics/cloud.png");
+	// 3 new sprites with the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+	// Position the clouds on teh left of the screen
+	// at different heights
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 150);
+	spriteCloud3.setPosition(0, 300);
+	// Are the clouds currently on screen
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+	// How fast is each cloud?
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
 
 	while (window.isOpen())
 	{
@@ -49,6 +93,14 @@ int main()
 		window.clear();
 		// Draw our game scene here
 		window.draw(spriteBackground);
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+		// Draw the tree
+		window.draw(spriteTree);
+		// Draw the insect
+		window.draw(spriteBee);
 		// Show everything we just drew
 		window.display();
 	}
