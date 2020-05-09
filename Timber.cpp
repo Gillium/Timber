754e260074bd7ac29c3cb3b05e9d7c40d01a3845
Peter Piper <petermpiper@gmail.com>
2020-05-09 09:16:20 -0400

    Prepared the player, axe, gravestone and flying log sprites. Drew the player, axe, flying log and gravestone sprites to the scene
    Prepared the player, axe, gravestone and flying log sprites. Drew the player, axe, flying log and gravestone sprites to the scene

--- Timber.cpp
+++ Timber.cpp
@@ -147,6 +147,46 @@ int main()
 		branches[i].setOrigin(220, 20);
 	}
 
+	// Prepare the player
+	Texture texturePlayer;
+	texturePlayer.loadFromFile("graphics/player.png");
+	Sprite spritePlayer;
+	spritePlayer.setTexture(texturePlayer);
+	spritePlayer.setPosition(580, 720);
+
+	// The player starts on the left
+	side playerSide = side::LEFT;
+
+	// Prepare the gravestone
+	Texture textureRIP;
+	textureRIP.loadFromFile("graphics/rip.png");
+	Sprite spriteRIP;
+	spriteRIP.setTexture(textureRIP);
+	spriteRIP.setPosition(600, 860);
+
+	// Prepare the axe
+	Texture textureAxe;
+	textureAxe.loadFromFile("graphics/axe.png");
+	Sprite spriteAxe;
+	spriteAxe.setTexture(textureAxe);
+	spriteAxe.setPosition(700, 830);
+
+	// Line the ace up with the tree
+	const float AXE_POSITION_LEFT = 700;
+	const float AXE_POSITION_RIGHT = 1075;
+
+	// Prepare the flying log
+	Texture textureLog;
+	textureLog.loadFromFile("graphics/log.png");
+	Sprite spriteLog;
+	spriteLog.setTexture(textureLog);
+	spriteLog.setPosition(810, 720);
+
+	// Some other useful log related variables
+	bool logActive = false;
+	float logSpeedX = 1000;
+	float logSpeedY = -1500;
+
 	while (window.isOpen())
 	{
 		/*
@@ -368,7 +408,20 @@ int main()
 
 		// Draw the tree
 		window.draw(spriteTree);
-		// Draw the insect
+
+		// Draw the player
+		window.draw(spritePlayer);
+
+		// Draw the axe
+		window.draw(spriteAxe);
+
+		// Draw the flying log
+		window.draw(spriteLog);
+
+		// Draw the gravestone
+		window.draw(spriteRIP);
+
+		// Draw the bee
 		window.draw(spriteBee);
 
 		// Draw the score
